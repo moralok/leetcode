9450fddd57b2d93911d80da5910af10c63af9c8f
maowenrou <wrmao.official@outlook.com>
2020-07-06 12:08:36 +0800

    蓄水池抽样，链表随机节点

---
+++ reservoir_sampling/linked_list_random_node/Solution.java
@@ -0,0 +1,61 @@
+package reservoir_sampling.linked_list_random_node;
+
+import java.util.Random;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+
+    private ListNode head;
+
+    /** @param head The linked list's head.
+        Note that the head is guaranteed to be not null, so it contains at least one node. */
+    public Solution(ListNode head) {
+        this.head = head;
+    }
+    
+    /** Returns a random node's value. */
+    public int getRandom() {
+        int count = 0;
+        int reserve = 0;
+        ListNode cur = this.head;
+        Random random = new Random();
+        while (cur != null) {
+            count++;
+            int rand = random.nextInt(count) + 1;
+            if (rand == count) {
+                reserve = cur.val;
+            }
+            cur = cur.next;
+        }
+        return reserve;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        Solution solution = new Solution(head);
+        for (int i = 0; i < 10; i++) {
+            System.out.println(solution.getRandom());
+        }
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(head);
+ * int param_1 = obj.getRandom();
+ */
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
