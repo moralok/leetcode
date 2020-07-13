de1327ebb6c382656f1be5776656903fa28cdb08
maowenrou <wrmao.official@outlook.com>
2020-07-13 10:58:39 +0800

    字符串，415，字符串相加，双指针法（注意进位，索引溢出）

---
+++ string/add_strings/Solution.java
@@ -0,0 +1,29 @@
+package string.add_strings;
+
+public class Solution {
+
+    public String addStrings(String num1, String num2) {
+        StringBuilder sb = new StringBuilder();
+        int i = num1.length() - 1, j = num2.length() - 1, carry = 0;
+        while (i >= 0 || j >= 0) {
+            int n1 = i >= 0 ? num1.charAt(i) - '0' : 0;
+            int n2 = j >= 0 ? num2.charAt(j) - '0' : 0;
+            int tmp = n1 + n2 + carry;
+            carry = tmp / 10;
+            sb.append(tmp % 10);
+            i--;
+            j--;
+        }
+        if (carry == 1) {
+            sb.append(1);
+        }
+        return sb.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String str1 = "99999999";
+        String str2 = "111";
+        System.out.println(solution.addStrings(str1, str2));
+    }
+}
