cb380e1ec7ef3861106bfc5897dc11e330fe365c
maowenrou <wrmao.official@outlook.com>
2020-07-23 16:21:21 +0800

    栈，225，用队列实现栈，完全理解错了。。双队列 and 单队列

---
+++ stack/implement_stack_using_queues/MyStack.java
@@ -0,0 +1,33 @@
+package stack.implement_stack_using_queues;
+
+import java.util.LinkedList;
+
+public class MyStack {
+
+    LinkedList<Integer> queue;
+
+    /** Initialize your data structure here. */
+    public MyStack() {
+        queue = new LinkedList<>();
+    }
+    
+    /** Push element x onto stack. */
+    public void push(int x) {
+        queue.add(x);
+    }
+    
+    /** Removes the element on top of the stack and returns that element. */
+    public int pop() {
+        return queue.pollLast();
+    }
+    
+    /** Get the top element. */
+    public int top() {
+        return queue.peekLast();
+    }
+    
+    /** Returns whether the stack is empty. */
+    public boolean empty() {
+        return queue.isEmpty();
+    }
+}
