2644b4de6596bd2ed5224c2af674ea40abe1a27b
maowenrou <wrmao.official@outlook.com>
2020-07-23 15:23:39 +0800

    链表，160，相交链表，暴力法 and set and 双指针666牛

---
+++ linked_list/intersection_of_two_linked_lists/IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,9 @@
+package linked_list.intersection_of_two_linked_lists;
+
+public class IntersectionOfTwoLinkedLists {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        // 暴力法 O(mn) O(1)
+        // 哈希表法 O(m + n) O(m) 或 O(n)
+        return null;
+    }
+}
---
+++ linked_list/intersection_of_two_linked_lists/IntersectionOfTwoLinkedLists2.java
@@ -0,0 +1,13 @@
+package linked_list.intersection_of_two_linked_lists;
+
+public class IntersectionOfTwoLinkedLists2 {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode p = headA;
+        ListNode q = headB;
+        while (p != q) {
+            p = p == null ? headB : p.next;
+            q = q == null ? headA : q.next;
+        }
+        return p;
+    }
+}
