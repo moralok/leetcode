2162b2fa4f4a334eda976cf2a3c7d89426b6c461
moralok <moralok@163.com>
2020-08-01 15:59:43 +0800

    排序，148，排序链表，归并排序链表（cut merge dummyHead）

---
+++ sort/sort_list/SortList.java
@@ -0,0 +1,37 @@
+package sort.sort_list;
+
+public class SortList {
+    public ListNode sortList(ListNode head) {
+        // 空间复杂度不符合要求
+        // 自顶向下
+        if (head == null || head.next == null) {
+            // 不是返回 null
+            return head;
+        }
+        // 快慢指针法找中间点
+        ListNode fast = head.next, slow = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode tmp = slow.next;
+        slow.next = null;
+        ListNode left = sortList(head);
+        ListNode right = sortList(tmp);
+        ListNode cur = new ListNode(0);
+        ListNode res = cur;
+        // 有序链表合并
+        while (left != null && right != null) {
+            if (left.val < right.val) {
+                cur.next = left;
+                left = left.next;
+            } else {
+                cur.next = right;
+                right = right.next;
+            }
+            cur = cur.next;
+        }
+        cur.next = left == null ? right : left;
+        return res.next;
+    }
+}
---
+++ sort/sort_list/SortList2.java
@@ -0,0 +1,68 @@
+package sort.sort_list;
+
+public class SortList2 {
+    public ListNode sortList(ListNode head) {
+        // 自底向上
+        // 计算长度
+        // 真滴强，感觉好理解多了
+        int len = 0;
+        for (ListNode i = head; i != null; i = i.next) {
+            len++;
+        }
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        for (int i = 1; i < len; i *= 2) {
+            // 第二段的索引值是 j+i
+            // 获取头部
+            ListNode cur = dummy.next;
+            ListNode tail = dummy;
+            while (cur != null) {
+                // 左半部分头部
+                ListNode left = cur;
+                // 右半部分头部
+                ListNode right = cut(cur, i);
+                // 更新为新的头部
+                cur = cut(right, i);
+                tail.next = merge(left, right);
+                // 保持尾部
+                while (tail.next != null) {
+                    tail = tail.next;
+                }
+            }
+        }
+        return dummy.next;
+    }
+
+    // merge(l1, l2)
+    private ListNode merge(ListNode left, ListNode right) {
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        while (left != null && right != null) {
+            if (left.val < right.val) {
+                cur.next = left;
+                left = left.next;
+            } else {
+                cur.next = right;
+                right = right.next;
+            }
+            cur = cur.next;
+        }
+        cur.next = left == null ? right : left;
+        return dummy.next;
+    }
+
+    // cut(l, n)
+    private ListNode cut(ListNode cur, int i) {
+        ListNode p = cur;
+        while (p != null && --i > 0) {
+            p = p.next;
+        }
+        if (p == null) {
+            return null;
+        }
+        ListNode next = p.next;
+        // 断开
+        p.next = null;
+        return next;
+    }
+}
