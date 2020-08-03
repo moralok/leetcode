15ed3e6e57d6fade7e8aceb389348b8783f3b08d
maowenrou <wrmao.official@outlook.com>
2020-08-03 18:04:33 +0800

    剑指 offer - 014-I - 剪绳子
    题解里大佬的数学推导也太帅了

---
+++ lcof/lcof014_i/Lcof014ISolution.java
@@ -0,0 +1,18 @@
+package lcof.lcof014_i;
+
+public class Lcof014ISolution {
+    public int cuttingRope(int n) {
+        // 数学推导
+        if (n <= 3) {
+            return n - 1;
+        }
+        int a = n / 3, b = n % 3;
+        if (b == 0) {
+            return (int)Math.pow(3, a);
+        }
+        if (b == 1) {
+            return (int)Math.pow(3, a - 1) * 4;
+        }
+        return (int)Math.pow(3, a) * 2;
+    }
+}
