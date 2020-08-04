5db71470d840feb137c1a2be003e5e984e367096
maowenrou <wrmao.official@outlook.com>
2020-08-04 13:58:09 +0800

    剑指 offer - 018 - 删除链表的节点
    dummyHead

---
+++ lcof/lcof018/Lcof018Solution.java
@@ -0,0 +1,18 @@
+package lcof.lcof018;
+
+public class Lcof018Solution {
+    public ListNode deleteNode(ListNode head, int val) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode pre = dummy;
+        while (pre.next != null) {
+            if (pre.next.val == val) {
+                pre.next = pre.next.next;
+                break;
+            } else {
+                pre = pre.next;
+            }
+        }
+        return dummy.next;
+    }
+}
