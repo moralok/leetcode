d81ab227b5bcd40b85efff741467947c23bc3442
maowenrou <wrmao.official@outlook.com>
2020-08-04 17:25:01 +0800

    剑指 offer - 025 - 合并两个排序的链表
    dummyHead

---
+++ lcof/lcof025/Lcof025Solution.java
@@ -0,0 +1,20 @@
+package lcof.lcof025;
+
+public class Lcof025Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val > l2.val) {
+                tail.next = l2;
+                l2 = l2.next;
+            } else {
+                tail.next = l1;
+                l1 = l1.next;
+            }
+            tail = tail.next;
+        }
+        tail.next = l1 == null ? l2 : l1;
+        return dummy.next;
+    }
+}
