793c03b8bd3b3dc6a24fc9cddccda6b01edebf15
maowenrou <wrmao.official@outlook.com>
2020-08-11 18:29:45 +0800

    剑指 offer - 059-I - 滑动窗口的最大值
    单调队列（好厉害）
    暴力法在面试样本不大的情况下可以通过

---
+++ lcof/lcof059_i/Lcof059ISolution.java
@@ -0,0 +1,32 @@
+package lcof.lcof059_i;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Lcof059ISolution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if (nums.length == 0 || k == 0) {
+            return new int[0];
+        }
+        int[] res = new int[nums.length - k + 1];
+        Deque<Integer> deque = new LinkedList<>();
+        for (int i = 0; i < k; i++) {
+            while (!deque.isEmpty() && deque.peekLast() < nums[i]) {
+                deque.removeLast();
+            }
+            deque.addLast(nums[i]);
+        }
+        res[0] = deque.peekFirst();
+        for (int i = k; i < nums.length; i++) {
+            if (deque.peekFirst() == nums[i - k]) {
+                deque.removeFirst();
+            }
+            while (!deque.isEmpty() && deque.peekLast() < nums[i]) {
+                deque.removeLast();
+            }
+            deque.addLast(nums[i]);
+            res[i - k + 1] = deque.peekFirst();
+        }
+        return res;
+    }
+}
