133d68507f00d1b44815c5e873467c74382b1177
maowenrou <wrmao.official@outlook.com>
2020-08-12 11:42:59 +0800

    剑指 offer - 065- 不用加减乘除做加法
    位运算

---
+++ lcof/lcof065/Lcof065Solution.java
@@ -0,0 +1,12 @@
+package lcof.lcof065;
+
+public class Lcof065Solution {
+    public int add(int a, int b) {
+        while (b != 0) {
+            int c = (a & b) << 1;
+            a ^= b;
+            b = c;
+        }
+        return a;
+    }
+}
