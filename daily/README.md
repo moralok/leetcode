f1be39bf33c4f5451b509041add60ea607e0e365
maowenrou <wrmao.official@outlook.com>
2020-10-14 18:29:02 +0800

    每日一题 20201014 1002 查找常用字符

---
+++ daily/README.md
@@ -0,0 +1,5 @@
+# 每日一题
+
+|题号|题目|难度|日期｜
+|----|----|----|----｜
+|[1002](https://leetcode-cn.com/problems/find-common-characters/)|[查找常用字符](lc1002)|简单|20201014|
---
+++ daily/lc1002/Lc1002Solution.java
@@ -0,0 +1,40 @@
+package daily.lc1002;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Lc1002Solution {
+    
+    public List<String> commonChars(String[] A) {
+        // 傻乎乎地想到了map
+        // 时间复杂度 O(n(M + E)，n为数组长度，M为字符串平均长度，E为字符集长度
+        int[] minFrequence = new int[26];
+        Arrays.fill(minFrequence, Integer.MAX_VALUE);
+        for (String word : A) {
+            int[] freq = new int[26];
+            for (int i = 0; i < word.length(); i++) {
+                char ch = word.charAt(i);
+                freq[ch - 'a']++;
+            }
+            for (int i = 0; i < 26; i++) {
+                minFrequence[i] = Math.min(minFrequence[i], freq[i]);
+            }
+        }
+        List<String> res = new LinkedList<>();
+        for (int i = 0; i < 26; i++) {
+            for (int j = 0; j < minFrequence[i]; j++) {
+                res.add(String.valueOf((char) (i + 'a')));
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Lc1002Solution solution = new Lc1002Solution();
+        String[] A = new String[] {"bella","label","roller"};
+        System.out.println(solution.commonChars(A));
+        A = new String[] {"cool","lock","cook"};
+        System.out.println(solution.commonChars(A));
+    }
+}
