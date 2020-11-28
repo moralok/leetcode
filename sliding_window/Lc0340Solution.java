e4b2bb5db44463062b609705dc128f956f77770d
moralok <moralok@163.com>
2020-11-28 22:51:27 +0800

    debug-修复优化时错误的改动

--- sliding_window/Lc0340Solution.java
+++ sliding_window/Lc0340Solution.java
@@ -1,5 +1,6 @@
 package sliding_window;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,11 +15,10 @@ public class Lc0340Solution {
         int left = 0, right = 0;
         while (right < n) {
             map.put(s.charAt(right), right++);
-            if (map.size() == k + 1) {
-                char start = s.charAt(left);
-                int min = map.get(start);
+            if (map.size() == k+1) {
+                int min = Collections.min(map.values());
+                map.remove(s.charAt(min));
                 left = min + 1;
-                map.remove(start);
             }
             ret = Math.max(ret, right - left);
         }
