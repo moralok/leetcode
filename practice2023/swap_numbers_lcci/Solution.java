832b847b4faa57bd8bf7dbd729cf9bf585357c94
moralok <wrmao.public@outlook.com>
2023-10-17 23:10:41 +0800

    位运算，面试题 16.01. 交换数字，中等。

---
+++ practice2023/swap_numbers_lcci/Solution.java
@@ -0,0 +1,11 @@
+package practice2023.swap_numbers_lcci;
+
+public class Solution {
+
+    public int[] swapNumbers(int[] numbers) {
+        numbers[0] = numbers[0] ^ numbers[1];
+        numbers[1] = numbers[0] ^ numbers[1];
+        numbers[0] = numbers[0] ^ numbers[1];
+        return numbers;
+    }
+}
