3aba617ddd2574a7ae4026c35b47e97cc13959b1
moralok <wrmao.public@outlook.com>
2024-02-15 23:48:18 +0800

    每日一题 20240215 0107 二叉树的层序遍历 II 中等

---
+++ daily/Daily20240215Lc0215Solution.java
@@ -0,0 +1,36 @@
+package daily;
+
+import common.TreeNode;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+public class Daily20240215Lc0215Solution {
+
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            res.add(0, list);
+        }
+        return res;
+    }
+}
