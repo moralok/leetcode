a03d711b7da5b40c56b5b2ab19cc2d74bfcde28d
moralok <wrmao.public@outlook.com>
2024-02-22 23:49:55 +0800

    每日一题 20240222 0889 根据前序和后序遍历构造二叉树 中等

---
+++ daily/Daily20240222Lc0889Solution.java
@@ -0,0 +1,34 @@
+package daily;
+
+import common.TreeNode;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Daily20240222Lc0889Solution {
+
+    public TreeNode constructFromPrePost(int[] preorder, int[] postorder) {
+        Map<Integer, Integer> indexMap = new HashMap<>();
+        for (int i = 0; i < postorder.length; i++) {
+            indexMap.put(postorder[i], i);
+        }
+        return dfs(preorder, 0, postorder.length - 1, postorder, 0, postorder.length - 1, indexMap);
+    }
+
+    private TreeNode dfs(int[] preorder, int preLeft, int preRight, int[] postorder, int postLeft, int postRight, Map<Integer, Integer> indexMap) {
+        if (preLeft > preRight) {
+            return null;
+        }
+        if (preLeft == preRight) {
+            return new TreeNode(preorder[preLeft]);
+        }
+        TreeNode node = new TreeNode(preorder[preLeft]);
+        int nextVal = preorder[preLeft + 1];
+        Integer index = indexMap.get(nextVal);
+        node.left = dfs(preorder, preLeft + 1, preLeft + index - postLeft + 1, postorder, postLeft, index, indexMap);
+        node.right = dfs(preorder, preLeft + index - postLeft + 2, preRight, postorder, index + 1, postRight - 1, indexMap);
+        return node;
+    }
+}
