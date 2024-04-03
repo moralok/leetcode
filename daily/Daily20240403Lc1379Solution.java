3aa1720cfeac798070994c2a0e7d9453deefeeae
moralok <wrmao.public@outlook.com>
2024-04-03 23:13:08 +0800

    每日一题 20240403 1379 找出克隆二叉树中的相同节点 简单

---
+++ daily/Daily20240403Lc1379Solution.java
@@ -0,0 +1,20 @@
+package daily;
+
+import common.TreeNode;
+
+public class Daily20240403Lc1379Solution {
+
+    public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
+        if (original == null) {
+            return null;
+        }
+        if (original.val == target.val) {
+            return cloned;
+        }
+        TreeNode left = getTargetCopy(original.left, cloned.left, target);
+        if (left != null) {
+            return left;
+        }
+        return getTargetCopy(original.right, cloned.right, target);
+    }
+}
